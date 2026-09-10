989da2b9ab79a031834c2e62a75890863a641364
JofielRamirez <jofiel.ramirez@uabc.edu.mx>
2026-09-10 01:48:35 -0700

    Initial commit Assignment 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+ plugins {
+    alias(libs.plugins.android.application)
+     alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.campusconnect"
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.campusconnect"
+        minSdk = 37
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2024.09.00")
+    implementation(composeBom)
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.runtime:runtime")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("com.google.android.material:material:1.12.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
